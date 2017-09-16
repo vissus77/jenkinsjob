c5e8fb69d55100fb55ecc6d84c8c4c73b7bb99cf
vissus77 <vissus77@gmail.com>
2017-09-16 17:25:05 +0530

    Update login.java

--- login.java
+++ login.java
@@ -2,7 +2,7 @@ class login
 {
 public static void main(String[] a)
 {
-System.out.println("Welocome to jenkins after modfication");
+System.out.println("Welocome to jenkins  toafter modfication");
 }
 }
 
